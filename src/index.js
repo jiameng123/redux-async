761cc2824e7873c96c141296d4fc4d70d7c2bdbf
jiameng <386805508@qq.com>
2019-03-15 00:12:34 +0800

    update index

--- src/index.js
+++ src/index.js
@@ -1,20 +1,33 @@
 import isPromise from 'is-promise';
 
-function  createRduxAsync(extraArgument) {
-    return ({dispatch, getState}) =>  next => action => {
-        console.log(action, 'action');
-        console.log(next, 'next')
 
-       return isPromise(action) ? action.then(dispatch): next(action)
+function  createRduxAsync(extraArgument) {
+    return ({dispatch, getState}) => next => action => {
+        
+        if(Object.prototype.toString.call(action)=== '[object GeneratorFunction]') {
+            const it = action(dispatch);
+
+            function handle(itRes) {
+                if(itRes.done) return;
+                const itValue = itRes.value;
+                if(isPromise(itValue)) {
+                    itValue
+                    .then(res => handle(it.next(res)))
+                    .catch(e => it.throw(e))
+                }
+            }
+
+            try {
+                handle(it.next());
+            } catch (error) {
+                it.throw(e)
+            }
+
+        }
+      
+       return next(action)
       
     }
-}
-
-
-function * getList(parmas) {
-    
-    const rest = yield axios.get();
-
 }
 
  const asyncChunk = createRduxAsync();
--- test/index.js
+++ test/index.js
@@ -1,6 +1,19 @@
 import chain from 'chain';
 import asynChunk from '../src';
 
+
 describe('test', () => {
-    
+    const doDispatch = (a) => { console.log(a+'action')};
+    const doGetState = () => {};
+
+    it('test', () => {
+        const action = asynChunk(doDispatch);
+        function *a (ccc) {
+            doDispatch('a');
+            const res = yield new Promise(res => res(1000));
+            doDispatch(res);
+        };
+        action(doDispatch)(a);
+      
+    });
 })
